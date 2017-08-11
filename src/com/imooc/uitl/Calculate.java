9f67e2859bfb2c17bf4cdfe51f73cdd3dada51e3
MrMagicalJ <864391385@qq.com>
2017-08-11 13:42:04 +0800

    Junit4Demo学习代码

---
+++ src/com/imooc/uitl/Calculate.java
@@ -0,0 +1,28 @@
+package com.imooc.uitl;
+/**
+ * 一个算法的类
+ * @author Administrator
+ *
+ */
+public class Calculate {
+
+	//加法
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	//减法
+	public int subtact(int a, int b) {
+		return a - b;
+	}
+
+	//乘法
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	//除法
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
---
+++ src/com/imooc/uitl/CalculateTest.java
@@ -0,0 +1,24 @@
+package com.imooc.uitl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * ≤‚ ‘À„∑®¿‡
+ * @author Administrator
+ *
+ */
+public class CalculateTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void add(){
+		
+	}
+
+}
